6a8407eaa6df96e6d59fce164015667ce1e6c08a
abFribourg <66437280+abFribourg@users.noreply.github.com>
2020-06-04 21:34:38 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { HomeComponent } from './home/home.component';
     AgmCoreModule.forRoot({
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-      apiKey: 'AIzaSyDDpyq_S7R73H3milYeY6IkSgMzEGR0P8o'
+      apiKey: ''
     })
   ],
   providers: [
